11bdf7018356f7887ba276f7fa149b41f8948ffc
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-04-07 20:29:05 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoute.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 .then(() =>{
-    console.log('Connencted to MongoDB');
+    console.log('Connected to MongoDB');
 })
 .catch((err) =>{
     console.log(err);
 });
 
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Mounting the userRoutes on '/api/user'
+app.use('/api/user', userRoutes);
 
 app.listen(3000, () =>{
     console.log('Server listening on port 3000!');
