fbb8dd273b707007a887cc4de5d027a51f6f5755
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-04-09 10:46:43 +0530

    created update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
 import authRoutes from './routes/authRoute.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -17,6 +18,8 @@ const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+app.use(cookieParser());
 
 
 app.use('/api/user', userRoutes);
